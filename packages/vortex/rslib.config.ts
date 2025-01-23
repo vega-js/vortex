876681d784d203bc77a6ba1b64a3c5ccbe241d42
AndreyZlobin <zlobin.andy@gmail.com>
2025-01-23 05:14:19 +0300

    chore: update build to cjs and mjs

---
+++ packages/vortex/rslib.config.ts
@@ -0,0 +1,57 @@
+// import { defineConfig, moduleTools } from '@modern-js/module-tools';
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  // plugins: [moduleTools()],
+  source: {
+    tsconfigPath: './tsconfig.build.json',
+    entry: {
+      index: [
+        'src/**/*.ts',
+        'src/**/*.tsx',
+
+        '!src/types.ts',
+        '!src/**/*.spec.ts',
+        '!src/**/*.spec.tsx',
+        '!src/**/*.test.ts',
+        '!src/**/*.test.tsx',
+      ],
+    },
+  },
+  output: {
+    minify: true,
+  },
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: 'esm',
+    },
+    {
+      bundle: false,
+      dts: true,
+      format: 'cjs',
+    },
+  ],
+  // buildConfig: {
+  //
+  //   platform: 'browser',
+  //   target: 'es2022',
+  //   minify: 'terser',
+  //   jsx: 'automatic',
+  //   splitting: true,
+  //   shims: false,
+  //   format: 'esm',
+  //   buildType: 'bundleless',
+  //   input: [
+  //     'src/**/*.ts',
+  //     'src/**/*.tsx',
+  //     '!src/types.ts',
+  //     '!src/**/*.spec.ts',
+  //     '!src/**/*.spec.tsx',
+  //     '!src/**/*.test.ts',
+  //     '!src/**/*.test.tsx',
+  //   ],
+  //   tsconfig: './tsconfig.build.json',
+  // },
+});
