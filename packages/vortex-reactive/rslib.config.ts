9d8a847e6c5eb1c55d804a6286ebab52f37e5967
AndreyZlobin <zlobin.andy@gmail.com>
2025-10-22 16:42:18 +0300

    chore: remove log

--- packages/vortex-reactive/rslib.config.ts
+++ packages/vortex-reactive/rslib.config.ts
@@ -4,12 +4,12 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   // plugins: [moduleTools()],
   source: {
+    tsconfigPath: './tsconfig.build.json',
     entry: {
       index: [
         'src/**/*.ts',
         'src/**/*.tsx',
 
-        '!src/types.ts',
         '!src/**/*.spec.ts',
         '!src/**/*.spec.tsx',
         '!src/**/*.test.ts',
@@ -24,34 +24,13 @@ export default defineConfig({
   lib: [
     {
       bundle: false,
-      dts: false,
+      dts: true,
       format: 'esm',
     },
     {
       bundle: false,
-      dts: false,
+      dts: true,
       format: 'cjs',
     },
   ],
-  // buildConfig: {
-  //
-  //   platform: 'browser',
-  //   target: 'es2022',
-  //   minify: 'terser',
-  //   jsx: 'automatic',
-  //   splitting: true,
-  //   shims: false,
-  //   format: 'esm',
-  //   buildType: 'bundleless',
-  //   input: [
-  //     'src/**/*.ts',
-  //     'src/**/*.tsx',
-  //     '!src/types.ts',
-  //     '!src/**/*.spec.ts',
-  //     '!src/**/*.spec.tsx',
-  //     '!src/**/*.test.ts',
-  //     '!src/**/*.test.tsx',
-  //   ],
-  //   tsconfig: './tsconfig.build.json',
-  // },
 });
